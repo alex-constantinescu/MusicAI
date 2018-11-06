5696dee4a4f1c292075be48130f361b3bf014ade
Alexandru Constantinescu <alexisb1996@gmail.com>
2018-11-06 18:35:43 +0200

    Added concept of scales.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const scales = {
+    ionian: [2, 2, 1, 2, 2, 2, 1],
+    dorian: [2, 1, 2, 2, 2, 1, 2],
+    phrygian: [1, 2, 2, 2, 1, 2, 2],
+    lydian: [2, 2, 2, 1, 2, 2, 1],
+    mixolydian: [2, 2, 1, 2, 2, 1, 2],
+    aeolian: [2, 1, 2, 2, 1, 2, 2],
+    locrian: [1, 2, 2, 1, 2, 2, 2]
+};
+
 const notes = ['C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5'];
 
 function getRandomInt(max) {
@@ -10,6 +20,19 @@ function getAudio(note) {
 
 function playChord(notes) {
     notes.map(getAudio).map(a => a.play());
+}
+
+function scaleToNotes(scale) {
+    let i = 0;
+    return_notes = scale.map(interval => {
+        let return_note = notes[i];
+        i += interval;
+        return return_note;
+    });
+
+    return_notes.push(notes[i]);
+
+    return return_notes;
 }
 
 function getRandomChord(n) {
@@ -26,7 +49,7 @@ function getRandomChord(n) {
     return note_indeces.map(note_index => notes[note_index]);
 }
 
-function playChordArpeggiated(notes, interval) {
+function playNotesArpeggiated(notes, interval) {
     let notes_audio = notes.map(getAudio);
 
     for (let i = 0; i < notes_audio.length; i++) {
@@ -34,20 +57,13 @@ function playChordArpeggiated(notes, interval) {
     }
 }
 
-function chordToString(chord) {
+function notesToString(chord) {
     return chord.join(' ');
 }
 
 function main() {
-    chords = {
-        Dm7: ['D4', 'F4', 'A4', 'C5'].reverse(),
-        Cmaj7: ['C4', 'E4', 'G4', 'B4', 'C5']
-    }
-
-    //playChordArpeggiated(chords.Cmaj7, 200);
-    //setTimeout(() => playChordArpeggiated(chords.Dm7, 250), 1500);
-
-    let chord = getRandomChord(3);
-    playChordArpeggiated(chord, 150);
-    console.log(chordToString(chord));
+    let scale = scales.dorian;
+    let scale_notes = scaleToNotes(scale);
+    console.log(notesToString(scale_notes));
+    playNotesArpeggiated(scale_notes, 200);
 }
