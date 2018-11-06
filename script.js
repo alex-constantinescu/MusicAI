0461419b40f4a3cda60b96f1d80144e10189990a
Alexandru Constantinescu <alexisb1996@gmail.com>
2018-11-06 18:55:37 +0200

    Another scale-related commit.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const scales = {
-    ionian: [2, 2, 1, 2, 2, 2, 1],
-    dorian: [2, 1, 2, 2, 2, 1, 2],
-    phrygian: [1, 2, 2, 2, 1, 2, 2],
-    lydian: [2, 2, 2, 1, 2, 2, 1],
-    mixolydian: [2, 2, 1, 2, 2, 1, 2],
-    aeolian: [2, 1, 2, 2, 1, 2, 2],
-    locrian: [1, 2, 2, 1, 2, 2, 2]
+    diatonic: {
+        ionian: [2, 2, 1, 2, 2, 2, 1],
+        dorian: [2, 1, 2, 2, 2, 1, 2],
+        phrygian: [1, 2, 2, 2, 1, 2, 2],
+        lydian: [2, 2, 2, 1, 2, 2, 1],
+        mixolydian: [2, 2, 1, 2, 2, 1, 2],
+        aeolian: [2, 1, 2, 2, 1, 2, 2],
+        locrian: [1, 2, 2, 1, 2, 2, 2]
+    },
+    misc: {
+        phrygian_dominant: [1, 3, 1, 2, 1, 2, 2],
+        hungarian_minor: [2, 1, 3, 1, 1, 3, 1],
+        romanian_minor: [2, 1, 3, 1, 2, 1, 2]
+    }
 };
 
 const notes = ['C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5'];
@@ -35,18 +42,18 @@ function scaleToNotes(scale) {
     return return_notes;
 }
 
-function getRandomChord(n) {
+function getRandomChord(n, scale) {
     let note_indeces = [];
 
     for (let i = 0; i < n; i++) {
         do {
-            note_index = getRandomInt(notes.length);
+            note_index = getRandomInt(scale.length);
         } while (note_indeces.indexOf(note_index) !== -1);
         note_indeces.push(note_index);
     }
 
     note_indeces.sort();
-    return note_indeces.map(note_index => notes[note_index]);
+    return note_indeces.map(note_index => scale[note_index]);
 }
 
 function playNotesArpeggiated(notes, interval) {
@@ -62,8 +69,11 @@ function notesToString(chord) {
 }
 
 function main() {
-    let scale = scales.dorian;
+    let scale = scales.misc.romanian_minor;
     let scale_notes = scaleToNotes(scale);
-    console.log(notesToString(scale_notes));
-    playNotesArpeggiated(scale_notes, 200);
+
+    let chord = getRandomChord(3, scale_notes);
+    console.log(notesToString(chord));
+
+    playNotesArpeggiated(scale_notes, 270);
 }
